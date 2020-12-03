9ff5c9c3e6b2c54ba02dd7c7409ad6cc9299c2ad
StepashaA <romanmotoru@gmail.com>
2020-12-03 19:47:12 +0300

    da da

---
+++ Assets/Scripts/manager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class manager : MonoBehaviour
+{
+    public GameObject Cube;
+
+    public GameObject Example;
+
+    public Transform SpawnPoint;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Inst();
+        }
+    }
+
+    void Inst()
+    {
+
+        Instantiate(Example, SpawnPoint.position, SpawnPoint.rotation, transform);
+
+    }
+
+    void EnableDisable()
+    {
+
+        Cube.SetActive(!Cube.activeSelf);
+
+    }
+
+    void ObjAndComp()
+    {
+
+    }
+}
